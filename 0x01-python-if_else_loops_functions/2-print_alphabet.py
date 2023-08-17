b90ce249879547b9051610d9e01a98d9d7e8821b
halezis <halezis007@gmail.com>
2023-08-17 08:36:24 +0100

    a program that prints ascii alphabets in small letters

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,10 @@
-for x in range(97, 123):
-    print(chr(x), end="")
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+# YOUR CODE HERE
+last_digit = number % 10
+if last_digit > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
+elif last_digit == 0:
+    print("Last digit of {} is {} and is 0".format(number, last_digit))elif last_digit < 6:
+    print("Last digit of {} is {} and is less than 6".format(number, last_digit))
