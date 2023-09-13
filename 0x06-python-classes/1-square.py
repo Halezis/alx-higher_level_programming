2ff0dcaeb57120a3e1eaa9a5d6fc0b97c38c59c3
Halezis <halezis007@gmail.com>
2023-09-13 09:43:35 +0100

    private square of size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module contains a class Square"""
+
+
+class Square:
+    """A square class"""
+
+    def __init__(self, size):
+        """Initializes the class instance
+        Args:
+            size(int): The size of the square
+        Returns: None
+        """
+        self.__size = size
