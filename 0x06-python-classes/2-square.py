4e904e838caed13106c97c55a5565cc8b334ff7c
Halezis <halezis007@gmail.com>
2023-09-13 10:09:18 +0100

    square size and error

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module contains a class Square"""
+
+class Square:
+    """A square class"""
+
+    def __init__(self, size=0):
+        """Initializes the class instance
+        Args:
+            size(int): The size of the square
+        Returns: None
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
